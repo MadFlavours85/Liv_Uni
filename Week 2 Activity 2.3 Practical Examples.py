4d19f4ff516a151a711ac4965ab32be6bafd142b
MadFlavours85 <123881360+MadFlavours85@users.noreply.github.com>
2023-02-16 13:31:52 +0000

    First Additions

---
+++ Week 2 Activity 2.3 Practical Examples.py
@@ -0,0 +1,46 @@
+#Write the below examples in a pep8 approved style
+
+# Assignment Operator w/ String
+greek_island = "Santorini"
+print(greek_island)
+
+#Pep8
+greek_island_2 = "Corfu"
+print(greek_island_2)
+#No change
+
+#Assignment operator with interger and tuple
+earth_age_bln = 4.4
+universe_age_bln = 14
+earth_age_bln += 0.1
+
+print (earth_age_bln)
+
+#Pep8
+#N/A
+
+#Assignment operator w/ list
+asia_wishlist = ["Bhutan", "Ha Long", "Laos", "Danxia", "Seoul", "Khao Sok", "Cebu", "Chiang Mai", "Ho Chi Minh"]
+
+#Pep8
+asia_wishlist_1 =   ["Bhutan", "Ha Long", "Laos",
+                    "Danxia", "Seoul", "Khao Sok",
+                    "Cebu", "Chiang Mai", "Ho Chi Minh "]
+print(asia_wishlist_1)
+
+
+#Relational comparison operator
+
+
+msg = "life is beautiful"
+if msg == "I love you":
+    print ("propose")
+else:
+    print ("wait xP")
+
+#Pep8
+msg = "life is beautiful"
+if msg == "I love you":
+    print("Propose")
+else:
+    print("BumbaClaat")
